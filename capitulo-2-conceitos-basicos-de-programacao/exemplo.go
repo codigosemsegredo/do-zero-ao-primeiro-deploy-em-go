b0b2e3f815f992ca1393ae497f6e47295d6e7b19
Código sem Segredo <codigosemsegredo@gmail.com>
2025-01-24 13:24:24 -0300

    Adicionado exemplo do Capitulo 2

---
+++ capitulo-2-conceitos-basicos-de-programacao/exemplo.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Maria"
+	idade := 28
+	altura := 1.65
+	programadora := true
+
+	fmt.Println("Nome:", nome)
+	fmt.Println("Idade:", idade)
+	fmt.Println("Altura:", altura)
+	fmt.Println("É programadora?", programadora)
+}
